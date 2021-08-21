9c9e0a5a77be1c868fa12d080996e6b9749b1090
zhengyong <zhengyong.js@chinatelecom.cn>
2021-08-21 09:19:03 +0800

    增加001、002的步骤，conda中新增包

---
+++ anaconda-202105-002.dockerfile
@@ -0,0 +1,3 @@
+FROM anaconda-202105-001:v1
+
+RUN conda install -y -c conda-forge lightgbm
--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
 docker build -f anaconda-202105-000.dockerfile -t anaconda-202105-000:v1 .
+docker build -f anaconda-202105-001.dockerfile -t anaconda-202105-001:v1 .
+docker build -f anaconda-202105-002.dockerfile -t anaconda-202105-002:v1 .
